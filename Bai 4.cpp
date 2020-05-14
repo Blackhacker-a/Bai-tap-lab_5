8ca992c81860b9ca5212c13f614ea96836beef6e
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-14 21:42:12 +0700

    e sửa bài 4
    e sửa lại bai 4

--- Bai 4.cpp
+++ Bai 4.cpp
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main(){
     int n;
+    int count=2;
 	do{
 	printf("Nhap vao so nguyen duong n ");
     scanf("%d",&n);	
 	}while(n<0);
     for(int i=2;i<n;i++){
     	if(n%i==0){
-		   printf("n khong la so nguyen to");
-		   break;
+		   count++;
         }
-        else{
-           printf("n la so nguyen to");
-		   break;	
-		}
-       
-        }
-    
+    }
+    if (count>2){
+    	printf("n la hop so");
+    	}
+    else{
+    	printf("n so nguyen to");
+	}	
     
 
 
